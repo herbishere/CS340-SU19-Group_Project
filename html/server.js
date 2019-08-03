7b2bbf2f11adb126a58279739623dca3e927f8c0
Herb <herbishere@users.noreply.github.com>
2019-08-03 18:24:04 -0500

    implemented SELECT functionality to teams

--- html/server.js
+++ html/server.js
@@ -53,9 +53,16 @@ app.get('/teams', function (req, res, next) {
 });
 
 app.get('/championships', function (req, res, next) {
-    res.render('championships', {
-        title: 'Championships'
-    })
+    var context = {};
+    mysql.pool.query("SELECT c.year, CONCAT(t1.team_city, ' ', t1.name) AS west_champ, c.west_record, CONCAT(t2.team_city, ' ', t2.name) AS east_champ, c.east_record, CONCAT(t3.team_city, ' ', t3.name) AS Winner FROM nba_championships AS c INNER JOIN nba_teams AS t1 ON t1.id = c.west_teamID INNER JOIN nba_teams AS t2 ON t2.id = c.east_teamID INNER JOIN nba_teams AS t3 ON t3.id = c.winner", function (err, rows, fields) {
+        if (err) {
+            next(err);
+            return;
+        }
+        context.series = rows;
+        context.title = 'Championships'
+        res.render('teams', context);
+    });
 });
 
 // ERROR HANDLING //
--- sql/nba_DMQ.sql
+++ sql/nba_DMQ.sql
@@ -22,16 +22,15 @@ UPDATE nba_teams SET team_city = :cityInput, name = :nameInput, conference = :co
 --- CHAMPIONSHIP ENTITY DATA MANIPULATION QUERIES
 ---
 
---- get championship information with team names based on year
+--- get championship information with team names
 SELECT c.year,
-CONCAT(t1.team_city, ' ', t1.name) AS West_Champ, c.west_record,
-CONCAT(t2.team_city, ' ', t2.name) AS East_Champ, c.east_record,
+CONCAT(t1.team_city, ' ', t1.name) AS west_champ, c.west_record,
+CONCAT(t2.team_city, ' ', t2.name) AS east_champ, c.east_record,
 CONCAT(t3.team_city, ' ', t3.name) AS Winner
 FROM nba_championships AS c
 INNER JOIN nba_teams AS t1 ON t1.id = c.west_teamID
 INNER JOIN nba_teams AS t2 ON t2.id = c.east_teamID
 INNER JOIN nba_teams AS t3 ON t3.id = c.winner
-WHERE year = :year_from_input
 
 --- add a new championship
 INSERT INTO nba_championships (year, west_teamID, west_record, east_teamID, east_record, winner)
