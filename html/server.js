114c28372aac4114eb267c3fadd5e4c9d1ee9920
Herb <herbishere@users.noreply.github.com>
2019-08-04 10:08:22 -0500

    renamed context.championsips to .series

--- html/server.js
+++ html/server.js
@@ -67,7 +67,7 @@ function getChampionshipInfo(res, mysql, context, complete) {
             res.write(JSON.stringify(error));
             res.end();
         }
-        context.championships = results;
+        context.series = results;
         complete();
     });
 }
@@ -84,15 +84,6 @@ app.get('/championships', function (req, res) {
             res.render('championships', context);
         }
     }
-    // mysql.pool.query("SELECT c.year, CONCAT(t1.team_city, ' ', t1.name) AS west_champ, c.west_record, CONCAT(t2.team_city, ' ', t2.name) AS east_champ, c.east_record, CONCAT(t3.team_city, ' ', t3.name) AS winner FROM nba_championships AS c INNER JOIN nba_teams AS t1 ON t1.id = c.west_teamID INNER JOIN nba_teams AS t2 ON t2.id = c.east_teamID INNER JOIN nba_teams AS t3 ON t3.id = c.winner", function (err, rows, fields) {
-    //     if (err) {
-    //         next(err);
-    //         return;
-    //     }
-    //     context.series = rows;
-    //     context.title = 'Championships'
-    //     res.render('championships', context);
-    // });
 });
 
 // ERROR HANDLING //
