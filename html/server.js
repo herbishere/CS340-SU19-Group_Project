5c03054225db04bfa991c986de534cd834e80cfe
Herb <herbishere@users.noreply.github.com>
2019-08-04 09:34:42 -0500

    testing different championship gets

--- html/server.js
+++ html/server.js
@@ -52,17 +52,47 @@ app.get('/teams', function (req, res, next) {
     });
 });
 
-app.get('/championships', function (req, res, next) {
-    var context = {};
-    mysql.pool.query("SELECT c.year, CONCAT(t1.team_city, ' ', t1.name) AS west_champ, c.west_record, CONCAT(t2.team_city, ' ', t2.name) AS east_champ, c.east_record, CONCAT(t3.team_city, ' ', t3.name) AS winner FROM nba_championships AS c INNER JOIN nba_teams AS t1 ON t1.id = c.west_teamID INNER JOIN nba_teams AS t2 ON t2.id = c.east_teamID INNER JOIN nba_teams AS t3 ON t3.id = c.winner", function (err, rows, fields) {
-        if (err) {
-            next(err);
-            return;
+/* CHAMPIONSHIPS PAGE REQUESTS 
+TODO:
+INSERT FUNCTIONALITY
+UPDATE FUNCTIONALITY
+
+FINISHED:
+SELECT FUNCTIONALITY
+*/
+
+function getChampionshipInfo(res, mysql, context, complete) {
+    mysql.pool.query("SELECT c.year, CONCAT(t1.team_city, ' ', t1.name) AS west_champ, c.west_record, CONCAT(t2.team_city, ' ', t2.name) AS east_champ, c.east_record, CONCAT(t3.team_city, ' ', t3.name) AS winner FROM nba_championships AS c INNER JOIN nba_teams AS t1 ON t1.id = c.west_teamID INNER JOIN nba_teams AS t2 ON t2.id = c.east_teamID INNER JOIN nba_teams AS t3 ON t3.id = c.winner", function (error, results, fields) {
+        if (error) {
+            res.write(JSON.stringify(error));
+            res.end();
         }
-        context.series = rows;
-        context.title = 'Championships'
-        res.render('championships', context);
+        context.championships = results;
+        complete();
     });
+}
+
+app.get('/championships', function (req, res) {
+    var callbackCount = 0;
+    var context = {};
+    context.title = 'Championships'
+    getChampionshipInfo(res, mysql, context, complete);
+
+    function complete() {
+        callbackCount++;
+        if (callbackCount >= 1) {
+            res.render('championships', context);
+        }
+    }
+    // mysql.pool.query("SELECT c.year, CONCAT(t1.team_city, ' ', t1.name) AS west_champ, c.west_record, CONCAT(t2.team_city, ' ', t2.name) AS east_champ, c.east_record, CONCAT(t3.team_city, ' ', t3.name) AS winner FROM nba_championships AS c INNER JOIN nba_teams AS t1 ON t1.id = c.west_teamID INNER JOIN nba_teams AS t2 ON t2.id = c.east_teamID INNER JOIN nba_teams AS t3 ON t3.id = c.winner", function (err, rows, fields) {
+    //     if (err) {
+    //         next(err);
+    //         return;
+    //     }
+    //     context.series = rows;
+    //     context.title = 'Championships'
+    //     res.render('championships', context);
+    // });
 });
 
 // ERROR HANDLING //
