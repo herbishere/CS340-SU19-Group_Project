755124f559646ab12fff514ce3fd12e0bec81a26
Herb <herbishere@users.noreply.github.com>
2019-08-05 10:21:50 -0500

    added another select query

--- sql/nba_DMQ.sql
+++ sql/nba_DMQ.sql
@@ -17,6 +17,9 @@ SELECT id AS team_id, CONCAT(team_city, ' ', name) AS team_name FROM nba_teams
 -- get all the division names
 SELECT DISTINCT division as division_name FROM nba_teams
 
+-- get specific teams information
+SELECT id, team_city, name, conference, division, arena, head_coach FROM nba_teams WHERE id = ?
+
 -- add a new team
 INSERT INTO `nba_teams` (`team_city`, `name`, `conference`, `division`, `arena`, `head_coach`) VALUES (?, ?, ?, ?, ?, ?)
 
