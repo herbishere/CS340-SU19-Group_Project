7840882021620a302176af0194badc4d81c16759
Herb <herbishere@users.noreply.github.com>
2019-08-05 11:18:39 -0500

    updated update query in dmq.sql

--- sql/nba_DMQ.sql
+++ sql/nba_DMQ.sql
@@ -24,7 +24,7 @@ SELECT id, team_city, name, conference, division, arena, head_coach FROM nba_tea
 INSERT INTO `nba_teams` (`team_city`, `name`, `conference`, `division`, `arena`, `head_coach`) VALUES (?, ?, ?, ?, ?, ?)
 
 -- update a team's data based on submission
-UPDATE nba_teams SET team_city = :cityInput, name = :nameInput, conference = :conferenceInput, division = :divisionInput, arena = :arenaInput, head_coach = :coachInput
+UPDATE `nba_teams` SET `team_city`=?, `name`=?, `conference`=?, `division`=?, `arena`=?, `head_coach`=? WHERE `id`=?
 
 ---
 --- CHAMPIONSHIP ENTITY DATA MANIPULATION QUERIES
