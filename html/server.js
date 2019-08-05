962d24ecedc401ebea5f98977293ab8b9a2ef3f2
Herb <herbishere@users.noreply.github.com>
2019-08-05 11:13:21 -0500

    go back to teams page after successfully updating

--- html/server.js
+++ html/server.js
@@ -239,8 +239,6 @@ app.get('/teams/:id', function (req, res) {
 
 // Update Values Based On Form
 app.post('/teams/:id', function (req, res) {
-    console.log(req.body);
-    console.log(req.params.id);
     var sql = "UPDATE `nba_teams` SET `team_city`=?, `name`=?, `conference`=?, `division`=?, `arena`=?, `head_coach`=? WHERE `id`=?";
     var conference = getConference(req.body.division);
     var inserts = [req.body.team_city, req.body.name, conference, req.body.division, req.body.arena, req.body.head_coach, req.params.id];
@@ -251,7 +249,7 @@ app.post('/teams/:id', function (req, res) {
             res.end();
         } else {
             res.status(200);
-            res.end();
+            res.redirect('/teams');
         }
     });
 });
