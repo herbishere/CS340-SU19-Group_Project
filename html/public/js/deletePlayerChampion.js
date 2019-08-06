3c687aa0d9624df806754231378f0b31603c9ac1
Herb <herbishere@users.noreply.github.com>
2019-08-05 20:04:18 -0500

    Revert "removed jquery"
    This reverts commit 42ce9be9b7007843a681d1aa2a3ef1dbfe0b6ac4.

--- html/public/js/deletePlayerChampion.js
+++ html/public/js/deletePlayerChampion.js
@@ -1,27 +1,28 @@
-function deletePlayerChampionship(championshipID, playerID) {
-    // $.ajax({
-    //     url: '/player_championships/year/' + championshipID + '/playerID/' + playerID,
-    //     type: 'DELETE',
-    //     success: function(result){
-    //         if (result.responseText != undefined) {
-    //             alert(result.responseText)
-    //         } else {
-    //             window.location.reload(true)
-    //         }
-    //     }
-    // })
-    var req = new XMLHttpRequest();
-    var payload = {
-        "championshipID": championshipID,
-        "playerID": playerID
-    }
-    var url = '/player_championships/year/' + championshipID + '/playerID/' + playerID
-    req.open("DELETE", url, true);
-    req.setRequestHeader("Content-Type", "application/json");
-    req.addEventListener("load", function () {
-        if (req.status < 200 && req.status >= 400) {
-            console.log("Error:" + req.statusText);
+function deletePlayerChampionship(championshipID, playerID){
+    $.ajax({
+        url: '/player_championships/year/' + championshipID + '/playerID/' + playerID,
+        type: 'DELETE',
+        success: function(result){
+            if (result.responseText != undefined) {
+                alert(result.responseText)
+            } else {
+                window.location.reload(true)
+            }
         }
-    });
-    req.send(JSON.stringify(payload));
+    })
+    // var req = new XMLHttpRequest();
+    // var payload = {
+    //     "championshipID": championshipID,
+    //     "playerID": playerID
+    // }
+    // req.open("DELETE", '/player_championships/year/' + championshipID + '/playerID/' + playerID, true);
+    // req.setRequestHeader("Content-Type", "application/json");
+    // req.addEventListener("load", function () {
+    //     if(req.status >=200 && req.status <400) {
+    //         window.location.reload(true);
+    //     } else {
+    //         console.log("Error:" + req.statusText);
+    //     }
+    // });
+    // req.send(JSON.stringify(payload));
 }
