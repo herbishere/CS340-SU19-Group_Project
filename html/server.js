8d45f792a8e4d9bd8bb796f5e9a56ac491000f4d
gilhernandez21 <49378316+gilhernandez21@users.noreply.github.com>
2019-08-11 19:12:52 -0500

    drop down using select query for teams in players page

--- html/server.js
+++ html/server.js
@@ -49,7 +49,16 @@ function getPlayerInfo(res, mysql, context, complete) {
     });
 }
 
-
+function getTeamsForPlayers(res, mysql, context, complete) {
+    mysql.pool.query('SELECT id, name FROM nba_teams', function (error, results, fields) {
+        if (error) {
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+        context.teamsForPlayers = results;
+        complete();
+    });
+}
 
 app.get('/players', function (req, res, next) {
 
@@ -57,11 +66,11 @@ app.get('/players', function (req, res, next) {
     var context = {};
     context.title = 'Players'
     getPlayerInfo(res, mysql, context, complete);
-
+    getTeamsForPlayers(res, mysql, context, complete);
 
     function complete() {
         callbackCount++;
-        if (callbackCount >= 1) {
+        if (callbackCount >= 2) {
             res.render('players', context);
         }
     }
